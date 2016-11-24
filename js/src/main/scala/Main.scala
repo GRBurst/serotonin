808593210f7667fd6ce7f96f8ce57fd860ae485b
Felix Dietze <felix@beerleader.net>
2016-11-24 18:38:44 +0100

    transitions

--- js/src/main/scala/Main.scala
+++ js/src/main/scala/Main.scala
@@ -3,6 +3,7 @@ package serotonin
 import scala.scalajs.js.JSApp
 import scala.scalajs.js
 import scala.scalajs.js.timers._
+import js.JSConverters._
 
 import org.singlespaced.d3js.Ops._
 import org.singlespaced.d3js.d3
@@ -16,32 +17,33 @@ object Main extends JSApp {
 
     def update(list: js.Array[Int]) {
       println(list)
-      val circles = svg.selectAll("circle").data(list)
+      val circles = svg.selectAll("circle").data(list, (d:Int) => d.toString)
 
       circles.enter()
         .append("circle")
+        .attr("r", 0)
+        .attr("cx", (d: Int) => d)
+        .attr("cy", 50)
+        // .style("opacity", 0)
+        .attr("fill", "green")
+
+      circles.transition().duration(1000)
         .attr("r", 10)
-        .attr("cx", (d: Int, i: Int) => d)
+        .attr("cx", (d: Int) => d)
         .attr("cy", 100)
+        // .style("opacity", 1)
+        .attr("fill", "black")
 
-      circles
-        .attr("cx", (d: Int, i: Int) => d)
-
-      circles.exit()
+      circles.exit().transition().duration(1000)
+        .attr("r", 0)
+        // .style("opacity", 0)
+        .attr("cy", 150)
+        .attr("fill", "red")
         .remove()
     }
 
-    setTimeout(2000) {
-      update(js.Array(1))
-    }
-    setTimeout(3000) {
-      update(js.Array(1, 20))
-    }
-    setTimeout(4000) {
-      update(js.Array(1))
-    }
-    setTimeout(5000) {
-      update(js.Array(30, 1))
+    setInterval(2000) {
+      update(Seq.fill(5)(20 + (util.Random.nextInt.abs % 5)*30 ).toJSArray)
     }
   }
 }
