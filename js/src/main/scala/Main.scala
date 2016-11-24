7e1901a82ea414613c34c8e40d70306a4e317091
Felix Dietze <felix@beerleader.net>
2016-11-24 23:56:03 +0100

    randomly moving spikes on graph

--- js/src/main/scala/Main.scala
+++ js/src/main/scala/Main.scala
@@ -22,8 +22,19 @@ object Main extends JSApp {
       .attr("width", width)
       .attr("height", height)
 
-    val nodes = js.Array(new Node(), new Node())
-    val links = js.Array(new Link(nodes(0), nodes(1)))
+    val nodes = js.Array(
+      new Node(50, 50),
+      new Node(60, 50),
+      new Node(70, 50),
+      new Node(100, 100)
+    )
+    val links = js.Array(
+      new Link(nodes(0), nodes(1)),
+      new Link(nodes(1), nodes(2)),
+      new Link(nodes(3), nodes(2)),
+      new Link(nodes(2), nodes(0)),
+      new Link(nodes(3), nodes(1))
+    )
 
     var force = d3.layout.force()
       .nodes(nodes)
@@ -31,30 +42,63 @@ object Main extends JSApp {
       .size(dimensions)
       .linkDistance(60)
 
-    val circles = svg.selectAll("circle")
-      .data(nodes)
-    val lines = svg.selectAll("line")
+    val synapses = svg.append("g").selectAll("line")
       .data(links)
+    val neurons = svg.append("g").selectAll("circle")
+      .data(nodes)
+    val spikes = svg.append("g")
 
-    lines.enter()
+    synapses.enter()
       .append("line")
       .attr("stroke", "gray")
 
-    circles.enter()
+    neurons.enter()
       .append("circle")
       .attr("r", 10)
-      .attr("fill", "green")
+      .attr("fill", "#7DBBFF")
+      .attr("stroke", "black")
+      .attr("stroke-width", "1")
 
     force.on("tick", { event =>
-      lines
+      synapses
         .attr("x1", (d: Link) => d.source.x)
         .attr("y1", (d: Link) => d.source.y)
         .attr("x2", (d: Link) => d.target.x)
         .attr("y2", (d: Link) => d.target.y)
 
-      circles
+      neurons
         .attr("cx", (d: Node) => d.x)
         .attr("cy", (d: Node) => d.y)
+    })
+
+    def sendSpike(source: Node, target: Node) {
+      spikes
+        .append("circle")
+        .attr("r", 5)
+        .attr("fill", "#FFE77D")
+        .attr("stroke", "black")
+        .attr("stroke-width", "1")
+        .attr("cx", source.x.get)
+        .attr("cy", source.y.get)
+        .transition().duration(300)
+        .attr("cx", target.x.get)
+        .attr("cy", target.y.get)
+        .remove()
+
+      setTimeout(300) {
+        val outgoing = links.filter(l => l.source == target)
+        val incoming = links.filter(l => l.target == target).map(l => new Link(l.target, l.source))
+        val all = outgoing ++ incoming
+        val link = all(util.Random.nextInt.abs % all.size)
+        sendSpike(link.source, link.target)
+      }
+    }
+
+    force.on("end", { event =>
+      val link = links(util.Random.nextInt.abs % links.size)
+      setTimeout(0) { sendSpike(link.source, link.target) }
+      setTimeout(100) { sendSpike(link.source, link.target) }
+      setTimeout(200) { sendSpike(link.source, link.target) }
     })
 
     force.start()
