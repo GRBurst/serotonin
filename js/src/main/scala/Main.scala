cce9ec72d031f9e5d5c06ab7e721b1e9b2e2bef8
Felix Dietze <felix@beerleader.net>
2016-11-26 23:09:16 +0100

    absolute position of lights

--- js/src/main/scala/Main.scala
+++ js/src/main/scala/Main.scala
@@ -24,17 +24,17 @@ object Main extends JSApp {
     lazy val system = ActorSystem("serotonin")
     import system.dispatcher // Execution Context
     system.scheduler.scheduleOnce(0 seconds) {
-      val lightA = system.actorOf(Props(new Light("#A8F5FF")))
-      val lightB = system.actorOf(Props(new Light("#FFEBA8")))
       val dimensions = Vec2(400, 300)
       import dimensions.{width, height}
+      val lightA = system.actorOf(Props(new Light("#A8F5FF", Vec2(width, height / 2 - 30))))
+      val lightB = system.actorOf(Props(new Light("#FFEBA8", Vec2(width, height / 2 + 30))))
       val visualisation = new Visualization(dimensions)
       val network = system.actorOf(Props(new VisNetwork(visualisation)))
 
-      network ! AddSensor(KeyboardSensorType("ArrowLeft"), Some(Vec2(30, dimensions.height / 2 - 30)))
-      network ! AddSensor(KeyboardSensorType("ArrowRight"), Some(Vec2(30, dimensions.height / 2 + 30)))
-      network ! AddMotor(lightA, Some(Vec2(dimensions.width - 30, dimensions.height / 2 - 30)))
-      network ! AddMotor(lightB, Some(Vec2(dimensions.width - 30, dimensions.height / 2 + 30)))
+      network ! AddSensor(KeyboardSensorType("ArrowLeft"), Some(Vec2(50, height / 2 - 30)))
+      network ! AddSensor(KeyboardSensorType("ArrowRight"), Some(Vec2(50, height / 2 + 30)))
+      network ! AddMotor(lightA, Some(Vec2(width - 50, height / 2 - 30)))
+      network ! AddMotor(lightB, Some(Vec2(width - 50, height / 2 + 30)))
 
       // system.scheduler.scheduleOnce(3.seconds, network, Signal(List(0.1, 0.2)))
       // system.scheduler.schedule(0.seconds, 0.315.second, network, Signal(List(0.1, 0.2)))
--- js/src/main/scala/Motors.scala
+++ js/src/main/scala/Motors.scala
@@ -12,18 +12,23 @@ import org.singlespaced.d3js.d3
 import org.singlespaced.d3js
 
 import akka.actor._
-
 import actor.Messages._
+import vectory._
 
-class Light(color: String) extends Actor {
+class Light(color: String, pos: Vec2) extends Actor {
   val bgColor = "#222222"
+  val border = 1
+  val size = Vec2(50, 50) + (2 * border)
+  val cornerpos = pos - size / 2
   val light = d3.select("#container").append("div")
   light
-    .style("display", "inline-block")
-    .style("margin", "2px")
-    .style("border", "1px solid black")
-    .style("width", "50px")
-    .style("height", "50px")
+    .style("position", "absolute")
+    .style("left", s"${cornerpos.x}px")
+    .style("top", s"${cornerpos.y}px")
+    .style("margin", "0px")
+    .style("border", s"${border}px solid black")
+    .style("width", s"${size.width}px")
+    .style("height", s"${size.height}px")
     .style("background", bgColor)
 
   def receive = {
